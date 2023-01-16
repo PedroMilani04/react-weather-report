21b4656cff19cf93843b47d390bf5613c70bd0f6
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2023-01-15 22:26:34 -0300

    v1 content 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { useState } from "react";
 import axios from "axios";
 function App() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid=7989b48507389ed58967d1ad0f43715f`
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+  const [country, setCountry] = useState('')
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location},&appid=7989b48507389ed58967d1ad0f43715f`
 
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
+  
   // começar app em baixo
   return (
     <div className="App">
+      <div className="search">
+        <input
+          className="loc"
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Insert Location"
+          type="text"
+        />
+
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            <p> California </p>
+            <p>{data.name}</p>
+            
           </div>
           <div className="temperature">
-            <h1>50°F</h1>
+            {data.main ? <h1>{(data.main.temp - 273.15).toFixed(2)}°C</h1> : null}
+            
           </div>
           <div className="weather-desc">
             <p>Sunny</p>
