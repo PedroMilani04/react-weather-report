1e0a1bc00a168922b927a71498c0a323a1a4982d
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2023-01-17 21:05:41 -0300

    added iframe map

--- src/App.js
+++ src/App.js
@@ -44,6 +44,18 @@ function App() {
           <div className="weather-desc">
             {data.weather ? <p>{(data.weather[0].description).charAt(0).toUpperCase() + (data.weather[0].description).slice(1)}</p> : null}
           </div>
+          <div className="weather-desc">
+            {data.weather ? <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d57706628.732183024!2d8.23477268926637!3d28.02040743149768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1673998184966!5m2!1spt-BR!2sbr"
+              
+              frameBorder="0"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              aria-hidden="false"
+              tabIndex="0"
+            /> : null}
+          </div>
+          
         </div>
         <div className="bottom">
           <div className="temperature-feels">
